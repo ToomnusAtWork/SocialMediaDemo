c4426cd799d91ff65e6ae4c412a060a7e79cd608
ToomnusAtWork <131127987+ToomnusAtWork@users.noreply.github.com>
2024-02-28 22:59:10 +0700

    add additional file and change small part

---
+++ src/assets/vectors/index.js
@@ -0,0 +1,8 @@
+import cloudService from './cloud.svg';
+import CodingScreen from './codingg.svg';
+import firewall from './firewall.svg';
+import databases from './databases.svg';
+import manual from './manual.svg';
+
+
+export {cloudService, CodingScreen, firewall, databases, manual};
